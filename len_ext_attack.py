4cc2e76c10efba0fa014be0ec11961c67dd3929e
Chengming Zhang <stvzhg@gmail.com>
2017-09-25 20:27:22 -0400

    remove unncecssary print

--- len_ext_attack.py
+++ len_ext_attack.py
@@ -10,14 +10,12 @@ query = parsedUrl.query
 params =  urlparse.parse_qs(query)
 token = params["token"][0]
 old = query[(len("token=")+len(token)+1):]
-print old
 
 h = md5(state=token.decode("hex"), count=512)
 pad = padding((len(old)+8)*8)
 qpad = urllib.quote(pad)
 h.update(c3)
 query = "token=" + h.hexdigest() + '&' + old + qpad + c3
-print h.hexdigest()
 
 conn = httplib.HTTPSConnection(parsedUrl.hostname,parsedUrl.port)
 conn.request("GET", parsedUrl.path + "?" + query)
